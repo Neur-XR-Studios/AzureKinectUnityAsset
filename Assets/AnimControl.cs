46a25c71f2c2cf5174de5ed8ef886db4aa91fc7b
Mohammed Jasim <42913015+jasimmj@users.noreply.github.com>
2022-04-27 14:12:46 +0530

    Multi-Scene update

---
+++ Assets/AnimControl.cs
@@ -0,0 +1,21 @@
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class AnimControl : MonoBehaviour
+{
+    private Animator anim;
+    public Slider slider;   //Assign the UI slider of your scene in this slot 
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("AnimControl", -1, slider.normalizedValue);
+    }
+}
---
+++ Assets/LoadScne.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScne : MonoBehaviour
+{
+    public int i;
+    public void Loadnoww()
+    {
+        SceneManager.LoadScene(i);
+    }
+
+}
